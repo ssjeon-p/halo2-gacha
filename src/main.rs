ba0b3019ff4b4fec01e09f7e5aaef9b036495b47
SJlaptop <ssjeon@postech.ac.kr>
2023-09-02 22:32:41 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,7 @@ impl<const MULTIPLIER: u64, const ADDER: u64> GachaConfig<MULTIPLIER, ADDER> {
 
         meta.enable_equality(adv_0);
         meta.enable_equality(adv_1);
+        meta.enable_equality(inst);
 
         meta.create_gate("linear congruence", |meta| {
             let x = meta.query_advice(adv_0, Rotation::cur());
@@ -54,7 +55,7 @@ impl<const MULTIPLIER: u64, const ADDER: u64> GachaConfig<MULTIPLIER, ADDER> {
 
             let s = meta.query_selector(selector);
 
-            vec![ s * (a * x + c - b - m * d) ]
+            vec![s * (a * x + c - b - m * d)]
         });
 
         GachaConfig {
@@ -65,35 +66,6 @@ impl<const MULTIPLIER: u64, const ADDER: u64> GachaConfig<MULTIPLIER, ADDER> {
         }
     }
 
-
-    
-
-    // fn assign_first_row(
-    //     &self,
-    //     mut layouter: impl Layouter<F>,
-    //     seed: u64
-    // ) -> Result<(ACell<F>, ACell<F>, ACell<F>), Error> {
-    //     layouter.assign_region(|| "first row with seed", |mut region| {
-    //         let offset = 0;
-
-    //         self.selector.enable(&mut region, offset)?;
-
-    //         let first_val = Value::known(F::from(seed));
-    //         let next = seed * MULTIPLIER + ADDER;
-    //         let next_quot = next / MODULUS;
-    //         let next_rem = next % MODULUS;
-    //         let quot_val = Value::known(F::from(next_quot));
-    //         let rem_val = Value::known(F::from(next_rem));
-
-    //         let first_cell = region.assign_advice(|| "seed", self.adv[0], offset, || first_val).map(ACell)?;
-    //         let next_cell = region.assign_advice(|| "next value mod m", self.adv[1], offset, || rem_val).map(ACell)?;
-    //         let quot_cell = region.assign_advice(|| "quotient", self.adv[1], offset, || quot_val).map(ACell)?;
-
-    //         Ok((first_cell, next_cell, quot_cell))
-    //     })
-    // }
-
-    
 
     fn assign_first_row(
         &self,
@@ -196,10 +168,10 @@ fn main() {
     let seed = 123;
     let circuit = GachaCircuit::<15, 3> {
         seed: Value::known(Fp::from(seed)),
-        n: 1,
+        n: 2,
     };
 
-    let public_input = vec![Fp::from(1)];
+    let public_input = vec![Fp::from(27723)];
     let prover = MockProver::run(4, &circuit, vec![public_input]).unwrap();
 
     prover.assert_satisfied();
